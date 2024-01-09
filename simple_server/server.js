00fdc95f4f385af4898282730ceb88367a18e353
bijay_ps <bijay.prakash7@yahoo.in>
2024-01-09 19:13:32 +0530

    Pilot commit

---
+++ simple_server/server.js
@@ -0,0 +1,15 @@
+const app = require("express");
+const axios = require("axios");
+
+const server = app();
+
+server.get("/users", (req, res) => {
+  axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
+    console.log(response.data);
+    res.send(200);
+  });
+});
+
+server.listen(3030, () => {
+  console.log("Server running on PORT 3030");
+});
